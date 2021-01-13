4455edadf1d1122e2df302b210b47d5e63e9c7eb
Sebastian Borg <Sebastian.borg99@hotmail.se>
2021-01-13 13:26:33 +0100

    Progress

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <form action="./results.php" method="post">
+            <input placeholder="Apor" type="number" name="apes">
+            <input placeholder="Giraffer" type="number" name="giraffe">
+            <input placeholder="Tigrar" type="number" name="tiger">
+            <input placeholder="Kokosnötter" type="number" name="coco">
+
+            <button>Spara</button>
+        
+        </form>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Result</title>
+</head>
+<body>
+
+<?php 
+
+    echo $_POST["apes"];
+    echo $_POST["giraffe"];
+    echo $_POST["tiger"];
+    echo $_POST["coco"];
+
+
+
+?>
+
+
+    
+</body>
+</html>
